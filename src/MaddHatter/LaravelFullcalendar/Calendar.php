c02c56be41027c7ce937ff59ca813365e3e9feb1
iginikolaev <iginikolaev@gmail.com>
2020-10-23 17:14:44 +0300

    Update Calendar.php

--- src/MaddHatter/LaravelFullcalendar/Calendar.php
+++ src/MaddHatter/LaravelFullcalendar/Calendar.php
@@ -2,6 +2,7 @@
 
 use ArrayAccess;
 use DateTime;
+use Illuminate\Support\Str;
 use Illuminate\View\Factory;
 
 class Calendar
@@ -126,7 +127,7 @@ class Calendar
             return $this->id;
         }
 
-        $this->id = str_random(8);
+        $this->id = Str::random(8);
 
         return $this->id;
     }
